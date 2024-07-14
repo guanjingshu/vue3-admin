4feccd73f8eb9e606d6b7e36e14f756963ec2d0a
guanjingshu <97731963+guanjingshu@users.noreply.github.com>
2024-07-14 23:15:35 +0800

    layout页面搭建

---
+++ src/layout/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="layout_container">
+    <div class="layout_slider">
+      <Logo></Logo>
+    </div>
+    <div class="layout_tabbar">456</div>
+    <div class="layout_main">789</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//引入左侧菜单logo子组件
+import Logo from "./logo/index.vue";
+</script>
+
+<style lang="scss" scoped>
+.layout_container {
+  width: 100%;
+  height: 100vh;
+
+  .layout_slider {
+    color: white;
+    width: $base-menu-width;
+    height: 100vh;
+    background: $base-menu-background;
+    transition: all 0.3s;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+
+      .el-menu {
+        border-right: none;
+      }
+    }
+  }
+
+  .layout_tabbar {
+    position: fixed;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabbar-height;
+    top: 0px;
+    left: $base-menu-width;
+    transition: all 0.3s;
+    &.fold {
+      width: calc(100vw - $base-menu-min-width);
+      left: $base-menu-min-width;
+    }
+  }
+
+  .layout_main {
+    position: absolute;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabbar-height);
+    left: $base-menu-width;
+    top: $base-tabbar-height;
+    padding: 20px;
+    overflow: auto;
+    transition: all 0.3s;
+
+    &.fold {
+      width: calc(100vw - $base-menu-min-width);
+      left: $base-menu-min-width;
+    }
+  }
+}
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,10 +5,33 @@ export const constantRoutes = [
     component: () => import("@/views/login/index.vue"),
   },
   {
+    //登录成功以后展示数据的路由
     path: "/",
-    name: "Home",
-    component: () => import("@/views/home/index.vue"),
+    component: () => import("@/layout/index.vue"),
+    name: "layout",
+    meta: {
+      title: "",
+      hidden: false,
+      icon: "",
+    },
+    redirect: "/home",
+    children: [
+      {
+        path: "/home",
+        component: () => import("@/views/home/index.vue"),
+        meta: {
+          title: "首页",
+          hidden: false,
+          icon: "HomeFilled",
+        },
+      },
+    ],
   },
+  // {
+  //   path: "/",
+  //   name: "Home",
+  //   component: () => import("@/views/home/index.vue"),
+  // },
   {
     path: "/404",
     name: "404",
